3c5a5771545c8c6f2254129e88264f21ea8d71c7
Kojirou <54878605+KameiKojirou@users.noreply.github.com>
2025-03-24 23:57:30 -0600

    menu and input example grow

--- examples/main_menu.go
+++ examples/main_menu.go
@@ -1,25 +1,28 @@
-// This file is all you need to start a project.
-// Save it somewhere, install the `gd` command and use `gd run` to get started.
 package examples
 
 import (
 	"graphics.gd/classdb"
 	"graphics.gd/classdb/Button"
+	"graphics.gd/classdb/Label"
 	"graphics.gd/classdb/Node"
+	"graphics.gd/classdb/SceneTree"
 	// "graphics.gd/classdb/SceneTree"
+	// "graphics.gd/classdb/Scene"
+	// "graphics.gd/classdb/ResourceLoader"
 )
 
 type MainMenu struct {
 	classdb.Extension[MainMenu, Node.Instance] `gd:"MainMenu"`
-	MyInputExampleButton                           Button.Instance
+	MyMenuLabel                                Label.Instance
+	MyInputExampleButton                       Button.Instance
 }
 
-
-
 func (m *MainMenu) Ready() {
+	m.MyMenuLabel.AsLabel().SetText("Grow Examples")
 	m.MyInputExampleButton.AsBaseButton().OnPressed(m.OnPressed)
 }
 
 func (m *MainMenu) OnPressed() {
-	// SceneTree.AsSceneTree().ChangeScene("res://input_example.tscn")
+	var tree SceneTree.Instance = m.Super().GetTree()
+	tree.ChangeSceneToFile("res://input_example.tscn")
 }
